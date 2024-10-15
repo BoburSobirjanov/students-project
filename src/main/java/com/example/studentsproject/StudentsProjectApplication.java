0e315ea7d84abfb91bfce736a1c17c1d7531e0ce
Bobur Sobirjanov <sobirjanovbobur768@gmail.com>
2024-10-15 11:41:00 +0500

    first commit

---
+++ src/main/java/com/example/studentsproject/StudentsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentsproject/StudentsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
