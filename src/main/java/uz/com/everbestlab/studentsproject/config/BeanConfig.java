8bf339234154dce6f541f3b8ca7abc32d1b86239
Bobur Sobirjanov <sobirjanovbobur768@gmail.com>
2024-10-16 15:08:33 +0500

    swagger added

--- src/main/java/uz/com/everbestlab/studentsproject/config/BeanConfig.java
+++ src/main/java/uz/com/everbestlab/studentsproject/config/BeanConfig.java
@@ -1,5 +1,9 @@
 package uz.com.everbestlab.studentsproject.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,4 +17,13 @@ public class BeanConfig {
         modelMapper.getConfiguration().setSkipNullEnabled(true);
         return modelMapper;
     }
+
+    @Bean
+    public OpenAPI customizeOpenApi(){
+        final String securitySchemeName= "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components());
+           }
 }
